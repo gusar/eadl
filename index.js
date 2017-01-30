b2837120c356e777d3cfb501396eb1cbdff3e79b
Andy Lahs <andy.lahs@gmail.com>
2017-01-30 14:46:04 +0000

    Add massivejs and postgres endpoints

--- index.js
+++ index.js
@@ -4,24 +4,61 @@ let massive = require('massive');
 let app = express();
 let db = massive.connectSync({db : "pgguide"});
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log('listening on port 3000');
 });
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.send('HelloWorld');
 });
 
-app.get('/users', function(req, res) {
-    db.run('select * from users', function(err, results) {
+app.get('/users', function (req, res) {
+    db.users.find({}, function (err, results) {
         console.log(results);
         res.send(results)
     });
 });
 
-app.get('/users', function(req, res) {
-    db.run('select * from users', function(err, results) {
+app.get('/users/:id', function (req, res) {
+	let user_id = {
+		id: req.params.id
+	}
+
+    db.users.find(user_id, function (err, result) {
+		res.send(result);
+	})
+});
+
+app.get('/products', function (req, res) {
+    db.products.find({}, function (err, results) {
         console.log(results);
         res.send(results)
     });
+});
+
+app.get('/products/:id', function (req, res) {
+	let product_id = {
+		id: req.params.id
+	}
+
+    db.products.find(product_id, function (err, result) {
+		res.send(result);
+	})
+});
+
+app.get('/purchases', function (req, res) {
+    db.purchases.find({}, function (err, results) {
+        console.log(results);
+        res.send(results)
+    });
+});
+
+app.get('/purchases/:id', function (req, res) {
+	let purchase_id = {
+		id: req.params.id
+	}
+
+    db.purchases.find(purchase_id, function (err, result) {
+		res.send(result);
+	})
 });
